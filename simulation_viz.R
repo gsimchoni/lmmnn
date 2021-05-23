5ceae588bcf3cac4112c06912d35239bc02a0dc5
gsimchoni <gsimchoni@gmail.com>
2021-05-23 14:00:32 +0300

    Updates celeba results

--- simulation_viz.R
+++ simulation_viz.R
@@ -84,8 +84,8 @@ airbnb <- read_csv("data/data_for_viz/airbnb_y_test.csv")
 airbnb_ns <- read_csv("data/data_for_viz/airbnb_ns.csv")
 drugs <- read_csv("data/data_for_viz/drugs_y_test.csv")
 drugs_ns <- read_csv("data/data_for_viz/drugs_ns.csv")
-ukb_images <- read_csv("data/data_for_viz/ukb_images_y_test.csv")
-ukb_images_ns <- read_csv("data/data_for_viz/ukb_images_ns.csv")
+celeba <- read_csv("data/data_for_viz/celeba_y_test.csv")
+celeba_ns <- read_csv("data/data_for_viz/celeba_ns.csv")
 
 ukbjob_scatter_y <- ukb_job %>%
   ggplot(aes(y_true, y_pred)) +
@@ -143,25 +143,25 @@ drugs_hist_b <- drugs_ns %>%
         plot.title = element_text(hjust = 0.5, size = 20),
         plot.subtitle = element_text(hjust = 0.5, size = 14))
 
-ukbimages_scatter_y <- ukb_images %>%
+celeba_scatter_y <- celeba %>%
   ggplot(aes(y_true, y_pred)) +
   geom_point(alpha = 0.2) +
   labs(x = "True test y", y = "") +
-  xlim(40, 70) +
-  ylim(40, 70) +
+  xlim(55, 125) +
+  ylim(55, 125) +
   # geom_abline(intercept = 0, slope = 1) +
   theme_bw() +
   theme(aspect.ratio=1, text = element_text(family = "Century", size=16))
 
-ukbimages_hist_b <- ukb_images_ns %>%
+celeba_hist_b <- celeba_ns %>%
   ggplot(aes(n)) +
   geom_histogram(alpha = 0.5) +
-  labs(x = "Category size", y = NULL, title = "UKB Age", subtitle = "Categorical: patient,  Y: age") +
+  labs(x = "Category size", y = NULL, title = "CelebA", subtitle = "Categorical: identity,  Y: noseX") +
   theme_bw() +
   theme(aspect.ratio=1, text = element_text(family = "Century", size=16),
         plot.title = element_text(hjust = 0.5, size = 20),
         plot.subtitle = element_text(hjust = 0.5, size = 14))
 
-p2 <- (ukbjob_hist_b / ukbjob_scatter_y) | (drugs_hist_b / drugs_scatter_y) | (ukbimages_hist_b / ukbimages_scatter_y) | (airbnb_hist_b / airbnb_scatter_y)
+p2 <- (ukbjob_hist_b / ukbjob_scatter_y) | (drugs_hist_b / drugs_scatter_y) | (celeba_hist_b / celeba_scatter_y) | (airbnb_hist_b / airbnb_scatter_y)
 
 ggsave("real_viz.png", p2, device = "png", width = 16, height = 8, dpi = 300)
