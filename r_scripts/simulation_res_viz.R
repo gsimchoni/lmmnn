819225d97a16e187a22302bcd15a39558edd9123
gsimchoni <gsimchoni@gmail.com>
2021-11-16 09:07:22 +0200

    Adds two categoricals viz code

--- r_scripts/simulation_res_viz.R
+++ r_scripts/simulation_res_viz.R
@@ -199,3 +199,66 @@ p <- (p1 | p2) / (p3 | p4)
 
 
 ggsave("real_data_res.png", p, device = "png", width = 14, height = 7, dpi = 300)
+
+#### Multiple Categoricals
+
+res <- read_csv("results/res_NL_100K_Z_n5_two_categoricals.csv")
+
+exp_type_label_list <- c(Ignore = "ignore", OHE = "ohe",
+                         Embeddings = "embed", lme4 = "lme4",
+                         LMMNN = "lmm")
+
+qs_label_list = c(
+  "q[1] == 1000 ~ q[2] == 1000" = "1000_1000",
+  "q[1] == 1000 ~ q[2] == 10000" = "1000_10000",
+  "q[1] == 10000 ~ q[2] == 10000" = "10000_10000"
+)
+label_parsed(names(qs_label_list))
+
+sig2bs_label_list = c(
+  "sigma[b1]^2 == 0.5 ~ sigma[b2]^2 == 0.5" = "0.5_0.5",
+  "sigma[b1]^2 == 0.5 ~ sigma[b2]^2 == 5" = "0.5_5",
+  "sigma[b1]^2 == 5 ~ sigma[b2]^2 == 5" = "5_5"
+)
+
+
+comb_levels <- tibble::tribble(
+      ~sig2bs, ~qs,
+   "0.5_0.5", "1000_1000",
+   "0.5_0.5", "1000_10000",
+   "0.5_0.5", "10000_10000",
+   "0.5_5", "1000_10000",
+   "0.5_5", "10000_10000",
+   "5_0.5", "1000_1000",
+   "5_5", "1000_1000",
+   "5_5", "1000_10000",
+   "5_5", "10000_10000"
+  )
+
+p <- 
+  res %>%
+  group_by(sig2b0, sig2b1, q0, q1, exp_type) %>%
+  summarise(MSE = mean(mse), se = sd(mse) / sqrt(5)) %>%
+  ungroup() %>%
+  unite("sig2bs", c(sig2b0, sig2b1)) %>%
+  unite("qs", c(q0, q1)) %>%
+  inner_join(comb_levels) %>%
+  mutate(sig2bs = ifelse(sig2bs == "5_0.5", "0.5_5", sig2bs)) %>%
+  mutate(exp_type = fct_relevel(exp_type, c("ignore", "ohe", "embed", "lme4", "lmm")),
+         exp_type = fct_recode(exp_type,
+                               !!!exp_type_label_list),
+         qs = fct_recode(as_factor(qs), !!!qs_label_list),
+         sig2bs = fct_recode(as_factor(sig2bs), !!!sig2bs_label_list)) %>%
+  ggplot(aes(exp_type, MSE, fill = factor(ifelse(exp_type == "LMMNN", "highlighted", "normal")))) +
+  geom_errorbar(aes(x = exp_type, ymin = MSE - se, ymax = MSE + se), width = 0.25) +
+  geom_bar(stat = "identity") +
+  scale_fill_manual(values=c("red", "grey50")) +
+  facet_grid(sig2bs ~ qs, scales = "free", labeller = label_parsed) +
+  labs(x = NULL, y = "Mean Test MSE") +
+  guides(fill = "none") +
+  theme_bw() +
+  theme(text = element_text(family = "Century", size=19),
+        axis.text.y = element_text(size = 12),
+        axis.text.x = element_text(angle = 45, hjust = 1))
+
+ggsave("res_NL_100K_Z_n5_two_categoricals.png", p, device = "png", width = 14, height = 7, dpi = 300)
